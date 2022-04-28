7a8a85bf9096c1e4ea7b8c1de0b5b2b0f0d7bb73
wilop <slayfel_ii@hotmail.com>
2022-04-27 23:40:51 -0600

    Recuperar usuario actual

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # proyecto2
 
-En base al primer proyecto se modifica usando el framework Bulma
+Proyecto de `html`, `css` y `javascript` para el curso _ISW-512 Diseño de Aplicaciones Web_ de la **Universidad Técnica Nacional** con el profesor _Rudy Barboza_ credo por _Wilberth López Aguilar_.
+
+Se base en **ProyectoI** el cual se modifica usando el framework Bulma.
 
 Para ver el repositorio de este proyecto en `GitHub` haga clic [aquí](https://github.com/wilop/proyectoI).
 
 
-Gracias 
+Gracias  
 :octocat:
--- js/dashboard.js
+++ js/dashboard.js
@@ -177,13 +177,55 @@ function saveRide() {
                 "<td class='is-hidden'>" + document.getElementById('friday').checked + "</td>" +
                 "<td class='is-hidden'>" + document.getElementById('saturday').checked + "</td>" +
                 "<td class='is-hidden'>" + document.getElementById('sunday').checked + "</td>" +
+                "<td class='is-hidden'>" + currentUser + "</td>" +
 
                 " </tr>";
+            saveRidesToLocalStorage(myTr[i]);
 
         }
     }
     changeTab('dashboard-tab', 'dashboard');
 
+}
+function saveRidesToLocalStorage(ride) {
+    var myRide = ride.getElementsByTagName('td');
+    alert(ride);
+
+    var ride = {
+        ridename: myRide[0].innerText,
+        star: myRide[1].innerText,
+        end: myRide[2].innerText,
+        // "<td> " +
+        // "<a href='#' onclick='viewRide()'>View</a> - " +
+        // "<a href='#' onclick='editRide()'>Edit</a> - " +
+        // "<a href='#' onclick='deleteRide(document.activeElement)'>Delete</a>" +
+        // "</td>" +
+        description: myRide[4].innerText,
+        departure: myRide[5].innerText,
+        arrival: myRide[6].innerText,
+        monday: myRide[7].innerText == 'true' ? true : false,
+        tuesday: myRide[8].innerText == 'true' ? true : false,
+        wednesday: myRide[9].innerText == 'true' ? true : false,
+        thursday: myRide[10].innerText == 'true' ? true : false,
+        friday: myRide[11].innerText == 'true' ? true : false,
+        saturday: myRide[12].innerText == 'true' ? true : false,
+        dsunday: myRide[13].innerText == 'true' ? true : false,
+        user: myRide[14].innerText == currentUser["username"],
+
+    };
+    if (typeof (Storage !== "undefined")) {
+        var valor = myRide;
+        var resgistro = JSON.parse(localStorage.getItem("rides"));
+        var arreglo = new Array();
+        if (resgistro == null) {
+            arreglo[0] = valor;
+        } else {
+            arreglo = resgistro;
+            arreglo[arreglo.length] = valor;
+        }
+        localStorage.setItem("rides", JSON.stringify(arreglo));
+        userRides.push(myRide);
+    }
 }
 function readData() {
     clearFormRides();
@@ -215,6 +257,9 @@ function readData() {
                 document.getElementById('friday').checked = myTd[11].innerText == 'true' ? true : false;
                 document.getElementById('saturday').checked = myTd[12].innerText == 'true' ? true : false;
                 document.getElementById('sunday').checked = myTd[13].innerText == 'true' ? true : false;
+                document.getElementById('user').checked = myTd[14].innerText == 'true' ? true : false;
+
+
             }
         }
     }
